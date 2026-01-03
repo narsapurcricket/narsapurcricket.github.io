ee95ae7aa9b47daeac90aae0c910b83a619d9b99
narsapurcricket <rajugundolla1@gmail.com>
2026-01-03 01:22:32 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    localStorage.setItem("isAdmin", "true");
+    alert("Admin login successful");
+    window.location.href = "index.html";
+  } catch {
+    alert("Invalid login");
+  }
+};
